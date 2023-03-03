03a54e34dfd8690a5d8f99e418ec0ca5549ab7f2
doggocaliper <125533492+doggocaliper@users.noreply.github.com>
2023-03-04 00:13:57 +0800

    Create question-6.py

---
+++ PCTC-round-1-sample-1.1/question-6.py
@@ -0,0 +1,18 @@
+name1 = str(input("Enter the 1st name: "))
+name2 = str(input("Enter the 2nd name: "))
+name3 = str(input("Enter the 3rd name: "))
+name4 = str(input("Enter the 4th name: "))
+name5 = str(input("Enter the 5th name: "))
+
+if "Ellie" == name1:
+    print("1st")
+elif "Ellie" == name2:
+    print("2nd")
+elif "Ellie" == name3:
+    print("3rd")
+elif "Ellie" == name4:
+    print("4th")
+elif "Ellie" == name5:
+    print("5th")
+else:
+    print("Name input was invalid and did not contain 'Ellie'.")
