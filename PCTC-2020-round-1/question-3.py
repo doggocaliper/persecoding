45316490c3f9b34b91df7743d0f9d75b5edb1302
doggocaliper <125533492+doggocaliper@users.noreply.github.com>
2023-03-04 13:59:27 +0800

    Create question-3.py

---
+++ PCTC-2020-round-1/question-3.py
@@ -0,0 +1,11 @@
+# Question 3
+
+food1 = str(input("Enter the 1st food: "))
+food2 = str(input("Enter the 2nd food: "))
+food3 = str(input("Enter the 3rd food: "))
+food4 = str(input("Enter the 4th food: "))
+
+shopping_list = f"{food1}, {food2}, {food3}, {food4}"
+
+print(food_list)
+print("_ " * len(food_list))
